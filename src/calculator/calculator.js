58257d913ff7ea5d3c364f136ef6466003c1ab99
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-10-06 09:52:41 +0100

    added functionality for subtract method to pass tests

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -3,7 +3,10 @@ export const calculator = {
     return num1 + num2;
   },
 
-  subtract(num1, num2) {},
+  subtract(num1, num2) {
+    return num1 - num2;
+  },
+
   divide(num1, num2) {},
   multiply(num1, num2) {},
 };
