399e094a25dc96874ad4b43fa18820190f4393d6
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-10-06 09:10:09 +0100

    Added more tests to capitalize.test.js and formatted stuff with prettier

--- src/capitalize/capitalize.test.js
+++ src/capitalize/capitalize.test.js
@@ -3,20 +3,40 @@ import { capitalize } from "./capitalize";
 const testWords = [
   {
     word: "test",
-    expected: "Test"
+    expected: "Test",
   },
   {
     word: "apple",
-    expected: "Apple"
+    expected: "Apple",
   },
   {
     word: "food",
-    expected: "Food"
+    expected: "Food",
+  },
+  {
+    word: "ORANGE",
+    expected: "ORANGE",
+  },
+  {
+    word: "$3!avocado*&%",
+    expected: "$3!Avocado*&%",
+  },
+  {
+    word: "1234",
+    expected: "1234",
+  },
+  {
+    word: "@#$%",
+    expected: "@#$%",
+  },
+  {
+    word: 1234,
+    expected: "Values should be of type string",
   },
 ];
 
-testWords.forEach(testCase => {
+testWords.forEach((testCase) => {
   test(`${testCase.word} should be capitalized`, () => {
     expect(capitalize(testCase.word)).toBe(testCase.expected);
-  })
+  });
 });
