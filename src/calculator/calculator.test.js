4e5e265723ab2adadbc5aa7a7dd276f5cb883883
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-10-06 09:52:21 +0100

    Added tests for subtract method

--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -11,3 +11,15 @@ test("2 + 2 should return 4", () => {
 test("12 + 13 should return 25", () => {
   expect(calculator.add(12, 13)).toBe(25);
 });
+
+test("8 - 3 should return 5", () => {
+  expect(calculator.subtract(8, 3)).toBe(5);
+});
+
+test("2 - 2 should return 0", () => {
+  expect(calculator.subtract(2, 2)).toBe(0);
+});
+
+test("12 - 13 should return -1", () => {
+  expect(calculator.subtract(12, 13)).toBe(-1);
+});
