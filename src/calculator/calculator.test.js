177472a145d36f0a4b774e65b74b8165ba5a53ae
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-10-08 10:43:53 +0100

    Refactor: removed repetition for tests by using describe.each and test.each more

--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -1,57 +1,36 @@
 import { expect } from "@jest/globals";
 import { calculator } from "./calculator";
 
-describe("add", () => {
-  test.each([
-    [5, 6, 11],
-    [2, 2, 4],
-    [12, 13, 25],
-  ])("%i + %i should return %i", (a, b, expected) => {
-    expect(calculator.add(a, b)).toBe(expected);
-  });
-});
-
-describe("subtract", () => {
-  test.each([
-    [8, 3, 5],
-    [2, 2, 0],
-    [12, 13, -1],
-  ])("%i - %i should return %i", (a, b, expected) => {
-    expect(calculator.subtract(a, b)).toBe(expected);
-  });
-});
-
-describe("divide", () => {
-  test.each([
+describe.each([
+  [calculator.add, [5, 6, 11], [2, 2, 4], [12, 13, 25]],
+  [calculator.subtract, [8, 3, 5], [2, 2, 0], [12, 13, -1]],
+  [calculator.multiply, [3, 3, 9], [-5, 2, -10], [0.7, 0.6, 0.42]],
+  [
+    calculator.divide,
     [9, 3, 3],
     [-4, 2, -2],
-    [1, 1, 1],
-    [8, 3, 2.67],
     [0.3, 0.2, 1.5],
     [5, 0, Infinity],
-  ])("%f / %f should return %f", (a, b, expected) => {
-    expect(calculator.divide(a, b)).toBe(expected);
-  });
-});
-
-describe("multiply", () => {
-  test.each([
-    [3, 3, 9],
-    [-5, 2, -10],
-    [0.7, 0.6, 0.42],
-  ])("%f * %f should return %f", (a, b, expected) => {
-    expect(calculator.multiply(a, b)).toBe(expected);
+  ],
+])("%s operation", (operation, ...cases) => {
+  test.each(cases)("%f and %f should return %f", (a, b, expected) => {
+    expect(operation(a, b)).toBe(expected, 2);
   });
 });
 
 describe("Invalid inputs", () => {
-  test.each([
-    ["hi", 0.3],
-    ["test", "hello"],
-  ])("should throw an error when inputs are %s and %s", (a, b) => {
-    expect(calculator.add(a, b)).toBe("One or both arguments are NaN.");
-    expect(calculator.subtract(a, b)).toBe("One or both arguments are NaN.");
-    expect(calculator.multiply(a, b)).toBe("One or both arguments are NaN.");
-    expect(calculator.divide(a, b)).toBe("One or both arguments are NaN.");
-  });
+  const operations = [
+    [calculator.add],
+    [calculator.subtract],
+    [calculator.multiply],
+    [calculator.divide],
+  ];
+
+  test.each(operations)(
+    "should throw an error for %s when inputs are not numbers",
+    (operation) => {
+      expect(operation("hi", 0.3)).toBe("One or both arguments are NaN.");
+      expect(operation("test", "hello")).toBe("One or both arguments are NaN.");
+    },
+  );
 });
